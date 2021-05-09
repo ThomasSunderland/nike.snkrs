4673ed609b3543cfe171545c34063f642ff04aee
ThomasSunderland <hsunderland@commandalkon.com>
2021-05-09 16:31:16 -0400

    Baseline Project/Repo

---
+++ app/src/main/java/com/nike/snkrs/sunderland/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nike.snkrs.sunderland
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
